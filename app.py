d8cd48357d85a0df61c91f12dc5ec34470b09c21
John <jbotonakis@carroll.edu>
2025-01-06 11:58:32 -0700

    If you're following along, no I do not know what I'm doing

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 #App.py
 #Author: John Botonakis
 #Desc:
-from flask import Flask
-
+from flask import Flask, render_template
 from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix='/views')
 
-# @app.route("/")
-# def index():
-#     return "Welcome to this web app!"
-app.register_blueprint(views, url_prefix='/')
-# app.register_blueprint(views, url_prefix='/home')
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+
 
 
 if __name__ == '__main__':
