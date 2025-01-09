9cb9278b24f6da29593e645803172634dc165b12
jbotonakis <jbotonakis@carroll.edu>
2025-01-09 11:36:10 -0700

    Added message flashing for input verification

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@
 #With help from "Tech With Tim" on Youtube
 #Desc:
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,flash
 
 auth = Blueprint('auth', __name__)
 
@@ -25,5 +25,22 @@ def logout():
 #This method can accept GET and POST requests
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    data = request.form
+    if request.method == "POST":
+        email = request.form.get('email_input')
+        first_name = request.form.get('firstname_input')
+        password1 = request.form.get('pass1_input')
+        password2 = request.form.get('pass2_input')
+
+        if len(email) < 4:
+            flash("Email must be at least 4 characters or greater", category="error")
+        elif len (first_name) < 3:
+            flash("First Name must be at least 3 characters or greater", category="error")
+        elif password1 != password2:
+            flash("Your passwords don't match!", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters or greater", category="error")
+        else:
+            #add the user
+            flash("Success! Account Created!", category="success")
+
     return render_template("signup.html")
