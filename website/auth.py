1f1f425f993c0fc688aeeb9c489de1c6f087289a
jbotonakis <jbotonakis@carroll.edu>
2025-01-09 16:44:56 -0700

    Addressed variable mismatch when logging in

--- website/auth.py
+++ website/auth.py
@@ -17,10 +17,10 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
    if request.method == 'POST':
-     email = request.form.get('email')
-     password = request.form.get('password')
+     email = request.form.get('email_login')
+     password = request.form.get('loginpass_login')
 
-     user = User.query.filter_by(email_login = email).first()
+     user = User.query.filter_by(email = email).first()
      if user:
          if check_password_hash(user.password, password):
              flash('Logged in successfully!', category='success')
