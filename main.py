4984bc2c66b8e78bf15aa10c232c3d037b359f39
John <jbotonakis@carroll.edu>
2025-03-12 10:58:35 -0600

    Begin Checkpoint 1

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-# Main.py
-# Author: John Botonakis
-# With some help from "Tech With Tim" on YouTube
-# Desc:
-# This is the entry point for the web app. It initializes the app
-# using the `create_app` function from the `website` package and
-# starts the development server.
-from website import create_app
+from flask import Flask, render_template
 
-# Creates an application using the model defined in the website folder
-# It then runs that created app.
-app = create_app()
+app = Flask(__name__)
 
-#FEEDBACK
-# [] Errors shouldn't be dismissible
-# [] Error for when pass don't match while signing up
-# [] Success message for account creation
-# [] Error when account is already created
-# [X] Remove "GET" from auth login
-# [] Allow logins to work
-
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    # Starts the development server
-    # `debug=True` enables debug mode, which provides detailed error messages
-    # and automatically reloads the server when code changes.
-    # Change to 'debug=False' when deploying.
     app.run(debug=True)
